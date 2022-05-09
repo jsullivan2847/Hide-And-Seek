647efc342c0f3879eb8f32f5fd57f19adcc44fcd
James Sullivan <jsullivan2847@gmail.com>
2022-05-09 17:11:01 -0400

    I've nearly reached MVP! Random location generator is set up, as well as markers stored as user answers and computer answers. I'm having a bit of trouble getting the actual coordinates out of the marker objects in number form but making progress and the only thing left to do is make a compare function and points system. Then onto CSS and design work

--- script.js
+++ script.js
@@ -1,110 +1,142 @@
 //VARIABLES................................................
+//storing answers and locations
 let markers = [];
+let answer = [];
 let randomLocations = [];
+//JQuery DOM element references
 const $deleteButton = $("#deleteBtn");
-const $newLocationButton = $('#newLocation')
+const $newLocationButton = $("#newLocation");
+const $mapView = $("#mapView");
 const $mapBox1 = $("#map1")[0];
 const $streetView = $("#streetView")[0];
 const googleUrl =
   "https://maps.googleapis.com/maps/api/js?key=AIzaSyBrL26kHgHcE6O9YC-F7mbxCXhwscpSsdA&callback=initMap";
 
-//BUTTONS..................................................
-$newLocationButton.on('click', function(){
-  getRandomLocation();
-  console.log(randomLocation)
-  console.log(randomLocations)
-  //console.log(randomLocation)
-})
-
-$deleteButton.on("click", function () {
-  console.log("clicked");
-  deleteMarker(marker);
-});
-
-//API CALL.................................................
+  //API CALL.................................................
 $.ajax({ url: googleUrl, dataType: "jsonp" }).then(function () {
   initMaps(brooklyn);
 });
 
-
-//Testing Locations
-//longitude and latitude range from 
-//-90 to 90 Lat
-//-180 to 180 long
-const brooklyn = { lat: 40.650002, lng: -73.949997 };
-const timesSquare = { lat: 40.758, lng: -73.9855 };
-
-
-
-//parameters for map are coordinates (in the form of an object)
-function initMaps(location) {
-  //MAP 1......STREET VIEW.........................
+//BUTTONS..................................................
+//Delete Marker
+$deleteButton.on("click", function () {
+  deleteMarker(marker);
+});
+//New Location
+$newLocationButton.on("click", function () {
   getRandomLocation();
-  console.log(randomLocation)
-  //map instantiation passing in DOM location and the actual *location*
   const map0 = new google.maps.Map($streetView, {
-    center: brooklyn,
+    center: randomLocation,
     zoom: 12,
     disableDefaultUI: true,
     zoomControl: false,
   });
-  //instantiates streetview settings
-  const streetView =  new google.maps.StreetViewPanorama($streetView, {
-    position: brooklyn,
+  const streetView = new google.maps.StreetViewPanorama($streetView, {
+    position: randomLocation,
     pov: {
       heading: 34,
       pitch: 10,
     },
-  })
-  //sets the map to streetview
-  // map0.setStreetView(streetView);
+  });
+  map0.getStreetView(streetView);
+  addMarker(randomLocation, map0);
+  console.log(randomLocation);
+});
+
+//Testing Locations
+//longitude and latitude range from
+//-90 to 90 Lat
+//-180 to 180 long
+const brooklyn = { lat: 40.650002, lng: -73.949997 };
+const gr = { lat: 42.963795, lng: -85.670006 };
+const timesSquare = { lat: 40.758, lng: -73.9855 };
+
+function getRandomLocation() {
+  //Right now it's set up to only give you locations in
+  //Brooklyn Queens and Manhattan
+  randomLocations = [];
+  let randomLat = (Math.random() * (40.75 - 40.59) + 40.59).toFixed(6) * 1;
+  let randomLong = (Math.random() * (-74 - -73.89) + -73.89).toFixed(6) * 1;
+  randomLocation = { lat: randomLat, lng: randomLong };
+  randomLocations.push(randomLocation);
+}
+
+//parameters for map are coordinates (in the form of an object)
+function initMaps(location) {
+  //MAP 1......STREET VIEW................................
+  getRandomLocation();
+
+  //map instantiation passing in DOM location and the actual *location*
+  const map0 = new google.maps.Map($streetView, {
+    center: randomLocation,
+    zoom: 12,
+    disableDefaultUI: true,
+    zoomControl: false,
+  });
+  addMarker(randomLocation, map0, "secret location", answer, false);
+  //for some reason I can't get the variables out of this array item
+  answerLocation = answer[0];
+  theAnswer = answerLocation.position
+  console.log("what im looking for", theAnswer)
+  console.log("secret location is ", answerLocation.toString());
+  //console.log('marker',map0.center)
+
+  //instantiates streetview settings
+  const streetView = new google.maps.StreetViewPanorama($streetView, {
+    position: randomLocation,
+    pov: {
+      heading: 34,
+      pitch: 10,
+    },
+  });
+  // sets the map to streetview
+  map0.getStreetView(streetView);
+
+  //Change Map View
+  $mapView.change(function () {
+    if (this.checked) {
+      streetView.setVisible(false);
+      //console.log(streetView.setVisible())
+    } else if (!this.checked) {
+      streetView.setVisible(true);
+    }
+  });
 
   //MAP 2...............................................
   //instantiate map on the right
   const map1 = new google.maps.Map($mapBox1, {
-    center: brooklyn,
+    center: map0.center,
     zoom: 12,
   });
   //adds a listener to the map for creating markers
   map1.addListener("click", function (e) {
-    addMarker(e.latLng, map1);
+    deleteMarker();
+    addMarker(e.latLng, map1, "Your Answer", markers, true);
+    console.log("answers array is at ", answer.length);
+    console.log("markers array is at ", markers.length);
+    let yourAnswer = markers[0].position.toString();
+    console.log("answer location: ", answerLocation.toString() )
+    console.log("your answer is ", parseInt(yourAnswer));
   });
 }
 
-
-
-//adding each new marker to an array, and then each time a new 
+//adding each new marker to an array, and then each time a new
 //marker is made, deleteMarker() is called, emptying the array
 //making it so you can only have 1 at a time
-function addMarker(latLng, map) {
-  marker = new google.maps.Marker({ 
-    position: latLng, 
+function addMarker(latLng, map, title, array, draggableChoice) {
+  marker = new google.maps.Marker({
+    position: latLng,
     map: map,
-    draggable: true,
-    title: 'drag me!',
-   });
-  deleteMarker();
-  markers.push(marker);
+    draggable: draggableChoice,
+    title: `${title}`,
+  });
+  array.push(marker);
   marker.setMap(map);
-  console.log(markers)
 }
 
-function deleteMarker(marker){
+function deleteMarker() {
   markers.forEach(function(marker){
-    marker.setMap(null);
-  })
+    marker.setMap(null)
+  });
   markers = [];
 }
-
-function getRandomLocation(){
-  randomLocations = [];
-  let randomLat = (Math.random() * (90-(-90) + 1) + (-90)).toFixed(6) * 1;
-  let randomLong = (Math.random()* (180-(-180) + 1) + (-180)).toFixed(6) * 1;
-  randomLocation = {lat: randomLat, lng: randomLong};
-  randomLocations.push(randomLocation);
-
-}
-
-console.log(randomLocations)
-
-
